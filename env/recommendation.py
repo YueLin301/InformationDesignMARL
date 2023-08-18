fix gumbel-softmax NaN

env/recommendation.py
@@ -16,7 +16,7 @@ class student_sampler(object):
     def __init__(self, prob_good, device):
         self.prob_good = prob_good
         self.prob_bad = 1 - prob_good
-        self.prob_distribution = torch.tensor([self.prob_bad, self.prob_good], dtype=torch.float32, device=device)
+        self.prob_distribution = torch.tensor([self.prob_bad, self.prob_good], dtype=torch.float64, device=device)
         self.sampler = torch.multinomial
 
     def sample(self, num=1):
@@ -29,8 +29,8 @@ class recommendation_env(object):
         self.config = config
         self.device = device
         self.student_sampler = student_sampler(prob_good=self.config.prob_good, device=device)
-        self.rewardmap_professor = torch.tensor(config.rewardmap_professor, dtype=torch.float32, device=device)
-        self.rewardmap_HR = torch.tensor(config.rewardmap_HR, dtype=torch.float32, device=device)
+        self.rewardmap_professor = torch.tensor(config.rewardmap_professor, dtype=torch.float64, device=device)
+        self.rewardmap_HR = torch.tensor(config.rewardmap_HR, dtype=torch.float64, device=device)
 
         assert config.sample_n_students > 0 and isinstance(config.sample_n_students, int)
         self.sample_n_students = config.sample_n_students

exp_recommendation/agent_class.py
@@ -8,11 +8,11 @@ class hr_class():
         self.device = device
 
         self.critic = torch.nn.Sequential(
-            torch.nn.Linear(in_features=4, out_features=2, bias=False, dtype=torch.float32), torch.nn.Tanh(),
-            torch.nn.Linear(in_features=2, out_features=1, bias=False, dtype=torch.float32)
+            torch.nn.Linear(in_features=4, out_features=2, bias=False, dtype=torch.float64), torch.nn.Tanh(),
+            torch.nn.Linear(in_features=2, out_features=1, bias=False, dtype=torch.float64)
         ).to(device)
         self.actor = torch.nn.Sequential(
-            torch.nn.Linear(in_features=2, out_features=2, bias=False, dtype=torch.float32),
+            torch.nn.Linear(in_features=2, out_features=2, bias=False, dtype=torch.float64),
             torch.nn.Softmax(dim=-1)
         ).to(device)
 

exp_recommendation/agent_formal_constrained.py
@@ -10,17 +10,17 @@ class pro_formal_constrained():
 
         # G^i(s,a), rather than Q(s,sigma)
         self.critic = torch.nn.Sequential(
-            torch.nn.Linear(in_features=4, out_features=2, bias=False, dtype=torch.float32), torch.nn.Tanh(),
-            torch.nn.Linear(in_features=2, out_features=1, bias=False, dtype=torch.float32)
+            torch.nn.Linear(in_features=4, out_features=2, bias=False, dtype=torch.float64), torch.nn.Tanh(),
+            torch.nn.Linear(in_features=2, out_features=1, bias=False, dtype=torch.float64)
         ).to(device)
         # G^j(s,a), rather than Q(sigma,a)
         self.critic_forhr = torch.nn.Sequential(
-            torch.nn.Linear(in_features=4, out_features=2, bias=False, dtype=torch.float32), torch.nn.Tanh(),
-            torch.nn.Linear(in_features=2, out_features=1, bias=False, dtype=torch.float32)
+            torch.nn.Linear(in_features=4, out_features=2, bias=False, dtype=torch.float64), torch.nn.Tanh(),
+            torch.nn.Linear(in_features=2, out_features=1, bias=False, dtype=torch.float64)
         ).to(device)
         self.signaling_net = torch.nn.Sequential(
             # input: one hot; output: signaling 0/1 prob. distribution
-            torch.nn.Linear(in_features=2, out_features=2, bias=False, dtype=torch.float32),
+            torch.nn.Linear(in_features=2, out_features=2, bias=False, dtype=torch.float64),
             torch.nn.Softmax(dim=-1)
         ).to(device)
 
@@ -34,7 +34,7 @@ class pro_formal_constrained():
 
         self.temperature = 1
         self.softmax_forGumble = torch.nn.Softmax(dim=-1)
-        self.message_table = torch.tensor([0, 1], dtype=torch.float32, device=self.device)
+        self.message_table = torch.tensor([0, 1], dtype=torch.float64, device=self.device)
 
         self.sender_objective_alpha = self.config.pro.sender_objective_alpha
 
@@ -80,7 +80,7 @@ class pro_formal_constrained():
         return
 
     def gumbel_sample(self, dim=2):
-        u = torch.rand(dim, dtype=torch.float32)
+        u = torch.rand(dim, dtype=torch.float64)
         g = - torch.log(-torch.log(u))
         return g
 
@@ -150,9 +150,9 @@ class pro_formal_constrained():
         sigma_counterfactual_onehot = 1 - sigma_onehot.detach()
         _, pi_counterfactual, _ = self.hr.choose_action(sigma_counterfactual_onehot)
 
-        a1 = torch.tensor([1, 0], dtype=torch.float32, device=self.device).unsqueeze(dim=0).repeat(
+        a1 = torch.tensor([1, 0], dtype=torch.float64, device=self.device).unsqueeze(dim=0).repeat(
             self.config.env.sample_n_students, 1)
-        a2 = torch.tensor([0, 1], dtype=torch.float32, device=self.device).unsqueeze(dim=0).repeat(
+        a2 = torch.tensor([0, 1], dtype=torch.float64, device=self.device).unsqueeze(dim=0).repeat(
             self.config.env.sample_n_students, 1)
         obs_and_a1 = torch.cat([obs_onehot, a1], dim=1)
         obs_and_a2 = torch.cat([obs_onehot, a2], dim=1)

exp_recommendation/analysis_payoff.py
@@ -4,25 +4,25 @@ from exp_recommendation.configs import env_config
 if __name__ == '__main__':
     p_pro0 = 0.1744
     p_pro1 = 0.3626
-    phi_bad = torch.tensor([[p_pro0, 1 - p_pro0]], dtype=torch.float32)
-    phi_good = torch.tensor([[p_pro1, 1 - p_pro1]], dtype=torch.float32)
+    phi_bad = torch.tensor([[p_pro0, 1 - p_pro0]], dtype=torch.float64)
+    phi_good = torch.tensor([[p_pro1, 1 - p_pro1]], dtype=torch.float64)
     phi = torch.cat([phi_bad, phi_good])
 
     p_hr0 = 1
     p_hr1 = 0
-    pi_not_rec = torch.tensor([[p_hr0, 1 - p_hr0]], dtype=torch.float32)
-    pi_rec = torch.tensor([[p_hr1, 1 - p_hr1]], dtype=torch.float32)
+    pi_not_rec = torch.tensor([[p_hr0, 1 - p_hr0]], dtype=torch.float64)
+    pi_rec = torch.tensor([[p_hr1, 1 - p_hr1]], dtype=torch.float64)
     pi = torch.cat([pi_not_rec, pi_rec])
 
-    prob_bad = torch.tensor(1 - env_config.config_env.prob_good, dtype=torch.float32)
-    p_stu = torch.tensor([1 - env_config.config_env.prob_good, env_config.config_env.prob_good], dtype=torch.float32)
+    prob_bad = torch.tensor(1 - env_config.config_env.prob_good, dtype=torch.float64)
+    p_stu = torch.tensor([1 - env_config.config_env.prob_good, env_config.config_env.prob_good], dtype=torch.float64)
 
     p_stu_2x2 = p_stu.unsqueeze(dim=1).repeat(1, 2)
     p_and_phi = p_stu_2x2 * phi
     p_and_pi = torch.mm(p_and_phi, pi)
 
-    rewardmap_pro_tensor = torch.tensor(env_config.config_env.rewardmap_professor, dtype=torch.float32)
-    rewardmap_hr_tensor = torch.tensor(env_config.config_env.rewardmap_HR, dtype=torch.float32)
+    rewardmap_pro_tensor = torch.tensor(env_config.config_env.rewardmap_professor, dtype=torch.float64)
+    rewardmap_hr_tensor = torch.tensor(env_config.config_env.rewardmap_HR, dtype=torch.float64)
 
     reward_pro_table = p_and_pi * rewardmap_pro_tensor
     reward_hr_table = p_and_pi * rewardmap_hr_tensor

exp_recommendation/rec_utils.py
@@ -17,7 +17,7 @@ def set_net_params(net, params):
 
 
 def int_to_onehot(variable_int_list, k, device):
-    variable_onehot = torch.zeros(len(variable_int_list), k, dtype=torch.float32, device=device)
+    variable_onehot = torch.zeros(len(variable_int_list), k, dtype=torch.float64, device=device)
     variable_onehot[range(len(variable_int_list)), variable_int_list] = 1
     return variable_onehot
 
@@ -68,7 +68,7 @@ def validate_critic(agent, device):
             input2_onehot = [0, 0]
             input1_onehot[input1] = 1
             input2_onehot[input2] = 1
-            input = torch.tensor(input1_onehot + input2_onehot, dtype=torch.float32, device=device)
+            input = torch.tensor(input1_onehot + input2_onehot, dtype=torch.float64, device=device)
             q = agent.critic(input)
             if agent.name == 'pro':
                 Gj = agent.critic_forhr(input)
@@ -89,7 +89,7 @@ def validate_sig_actor(agent, device):
         elif agent.name == 'hr':
             input_onehot = [0, 0]
             input_onehot[input] = 1
-            input_tensor = torch.tensor(input_onehot, dtype=torch.float32, device=device)
+            input_tensor = torch.tensor(input_onehot, dtype=torch.float64, device=device)
             _, pi, _ = agent.choose_action(input_tensor)
             print('signal:{}, pi:{}'.format(input, pi))
         else:
@@ -109,10 +109,10 @@ def validate(pro, hr, device):
 def unittest_which_weights_are_active(pro, hr):
     # 把actor的sigmoid层注释掉就知道了
     print_params(pro, hr)
-    message_onehot_temp1 = torch.tensor([1, 0], dtype=torch.float32)
-    message_onehot_temp2 = torch.tensor([0, 1], dtype=torch.float32)
+    message_onehot_temp1 = torch.tensor([1, 0], dtype=torch.float64)
+    message_onehot_temp2 = torch.tensor([0, 1], dtype=torch.float64)
     actor_parameters = list(hr.actor.parameters())
-    actor_parameters[0].data = torch.tensor([[1, 2], [3, 4]], dtype=torch.float32)
+    actor_parameters[0].data = torch.tensor([[1, 2], [3, 4]], dtype=torch.float64)
     print_params(pro, hr)
     _, pi1, _ = hr.choose_action(message_onehot_temp1)
     _, pi2, _ = hr.choose_action(message_onehot_temp2)
@@ -128,7 +128,7 @@ def permutation_test(pro, hr):
     for message_int in range(2):
         message_onehot = [0, 0]
         message_onehot[message_int] = 1
-        message_onehot = torch.tensor(message_onehot, dtype=torch.float32)
+        message_onehot = torch.tensor(message_onehot, dtype=torch.float64)
         _, pi, _ = hr.choose_action(message_onehot)
         print('pi(a|message={})=\t'.format(message_int), pi.tolist())
 